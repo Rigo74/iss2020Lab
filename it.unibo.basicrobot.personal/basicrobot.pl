82cddea0d3284dfe29c3c54c9bffc279239d1eaa
Stefano Righini <stefano.righini6@studio.unibo.it>
2019-10-17 14:32:15 +0200

    Refactor basicrobot.

---
+++ it.unibo.basicrobot.personal/basicrobot.pl
@@ -0,0 +1,7 @@
+%====================================================================================
+% basicrobot description   
+%====================================================================================
+context(ctxbasicrobot, "localhost",  "TCP", "8018").
+ qactor( robotadapter, ctxbasicrobot, "itunibo.robot.robotAdapterQa").
+  qactor( basicrobot, ctxbasicrobot, "it.unibo.basicrobot.Basicrobot").
+  qactor( sentinel, ctxbasicrobot, "it.unibo.sentinel.Sentinel").
