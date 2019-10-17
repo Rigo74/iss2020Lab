9de4ce996ce1dbcc74d535712a5dafbd77fefa3d
Stefano Righini <stefano.righini6@studio.unibo.it>
2019-10-17 14:35:59 +0200

    resolve some conflicts

--- it.unibo.basicRobot.personal/basicrobot.pl
+++
@@ -1,6 +0,0 @@
-%====================================================================================
-% basicrobot description   
-%====================================================================================
-context(ctxrobot, "localhost",  "TCP", "8018").
- qactor( robot, ctxrobot, "it.unibo.robot.Robot").
-  qactor( robotcallersimulator, ctxrobot, "it.unibo.robotcallersimulator.Robotcallersimulator").
